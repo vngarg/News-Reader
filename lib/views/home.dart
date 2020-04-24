1510b2bab1156438bd88d7e8e24d106549f113b5
vngarg <sngargrsd@gmail.com>
2020-04-24 20:33:56 +0530

    Styling added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -9,7 +9,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List<CategoryModel> categories = List<CategoryModel>();
   @override
   void initState() {
@@ -25,9 +24,10 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('S.G. '),
-            Text('News' , style: TextStyle(
-              color: Colors.blue
-            ),)
+            Text(
+              'News',
+              style: TextStyle(color: Colors.blue),
+            )
           ],
         ),
         elevation: 0.0,
@@ -36,17 +36,19 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
               height: 70.0,
               child: ListView.builder(
                 itemCount: categories.length,
                 shrinkWrap: true,
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context , index) {
+                itemBuilder: (context, index) {
                   return CategoryTile(
                     imageUrl: categories[index].imageUrl,
                     categoryName: categories[index].categoryName,
                   );
-                },),
+                },
+              ),
             )
           ],
         ),
@@ -56,20 +58,42 @@ class _HomeState extends State<Home> {
 }
 
 class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
 
-  final imageUrl , categoryName;
-
-  CategoryTile({
-    this.imageUrl , this.categoryName
-  });
+  CategoryTile({this.imageUrl, this.categoryName});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Image.network(imageUrl , width: 120.0 , height: 60.0,)
-        ],
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(right: 16.0),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6.0),
+                child: Image.network(
+                  imageUrl,
+                  width: 120.0,
+                  height: 60.0,
+                  fit: BoxFit.cover,
+                )),
+            Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    color: Colors.black26),
+                height: 60.0,
+                width: 120.0,
+                child: Text(
+                  categoryName,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14.0),
+                )),
+          ],
+        ),
       ),
     );
   }
