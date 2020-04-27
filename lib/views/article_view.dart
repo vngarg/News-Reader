aeef6b33ca5ba3b204df5c0305ae47c64c12e65e
vngarg <sngargrsd@gmail.com>
2020-04-27 10:47:02 +0530

    Complete News Display Added

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,23 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
-
   final String blogUrl;
-  ArticleView({
-    this.blogUrl
-  });
+  ArticleView({this.blogUrl});
 
   @override
   _ArticleViewState createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: WebView(
-        initialUrl: widget.blogUrl,
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('S.G. '),
+            Text(
+              'News',
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0.0,
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Icon(Icons.save)),
+          )
+        ],
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: ((WebViewController webViewController) {
+            _completer.complete(webViewController);
+          }),
+        ),
       ),
     );
   }
